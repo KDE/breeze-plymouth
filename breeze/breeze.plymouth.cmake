292cf8d39c6357ef37efae62007025f76d8fe0e5
Daniel Vrátil <dvratil@kde.org>
2016-03-06 14:25:20 +0100

    Don't hardcode path to Plymouth theme directory
    Replace paths in breeze.plymouth with the value of
    as that may differ from distro to distro
    
    REVIEW: 127295

---
+++ breeze/breeze.plymouth.cmake
@@ -0,0 +1,9 @@
+[Plymouth Theme]
+Name=Breeze
+Description=A Breeze Splash
+ModuleName=script
+
+[script]
+ImageDir=@Plymouth_THEMESDIR@/breeze
+ScriptFile=@Plymouth_THEMESDIR@/breeze/breeze.script
+
